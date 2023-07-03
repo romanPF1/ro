e75f9a5efd6166cd84ad332970e3b71813dcb8de
Олег <138213728+romanPF1@users.noreply.github.com>
2023-07-03 19:15:00 +0300

    comittsdas

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
-if __name__ == "__main__" :
-    
+if __name__ == '__main__':
+    # reverse
+    # print(faterName[::-1])
+    # print(arr[arr::-1])
+    # Tuple
+
+
+
+    # SER
+
+
+    # Primitive Date
+    a = 1
+    b = a
+    b += 1
+    print(a, b)
+
+    #Complex Date
+    c = [1]
+    d = c.copy()
+    d[0] += 1
+
+    print(c, d)
+    #print(c, d)
+
+    users = [
+        {
+            "name":     "John",
+            "age": 31
+        },
+        {
+            "name": "John",
+            "age": 15
+        },
+        {
+            "name": "John",
+            "age": 15
+        },
+        {
+            "name": "John",
+            "age": 31
+        },
+    ]
+    print(users)
+
+
