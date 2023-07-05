37a41c7e03616350b2406c800bce0e196a24c54d
Олег <138213728+romanPF1@users.noreply.github.com>
2023-07-05 19:24:48 +0300

    05.07.23

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-if __name__ == "__main__" :
+if __name__ == "__main__":
+    # kisnevo_riven = int(input("рівень кисню")
+    # temp = int(input("рівень температури"))
+    # if kisnevo_riven >= 18 and temp > 25 and temp < 35:
+    #    print("можна відкрити вікно")
+    # elif kisnevo_riven < 18 and temp < 25:
+    #   print("не можна відкрити вікно")
+
+  #  age = int(input("ваш вік"))
+  #  income = int(input("ваш річний дохід"))
+  #   employed_input = input("у вас є стабільне працевлаштування: ")
+  #  blacklisted_input =  input("ви знаходитесь у списку небажаних позичальників")
+  #  if employed_input.lower() == 'так':
+  #      employed = True
+  #  else:
+  #      employed = False
+  #  if blacklisted_input.lower() == 'ні':
+  #      blacklested = True
+  #  else:
+  #      blacklested = False
+  #  if  18 < age < 65 and income >= 25000:
+  #      print("вам допустимий кредит")
+  #  elif age < 18 and income < 25000:
+  #      print("вам недопустимий кредит")
+
+   # income = int(input("ваш річний дохід"))
+   # age = int(input("ваш вік"))
+   # if 18 < age < 25 and income < 20000:
+   #     print("вам надається студентський кредит")
+   # elif 26 < age < 40 and income < 20000:
+   #     print("вам надається особистий кредит")
+   # elif  41 < age < 60 and income > 40000:
+   #     print("вам надається поточний кредит")
+   # else:
+   #     print("кредит недопустимий")
+
+    age = int(input("ваш вік"))
+    income = int(input("ваш річний дохід"))
     
+
+
+
+
+
+
