1194ab5987a14c35232cd8f0d2fa92981dada68e
Олег <138213728+romanPF1@users.noreply.github.com>
2023-07-12 18:07:42 +0300

    sdfsdg

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
+import json
 if __name__ == "__main__" :
-    
+    with open("products.json", "r", encoding="utf-8") as file:
+        products = json.load(file)
+      #  total_price = 0
+      #  for item in products:
+
+       #     price = item['price'].replace("NBSP", "").replace(" ", "")
+       #     item[price] = price
+       #     total_price += int(price)
+      #  print(total_price)
+      #  new_list_products = []
+       # for item in products:
+           # print(item['colors'])
+        #    for color in (item['colors']):
+         #       print(color)
+          #      if color.lower() == "чорний":
+           #         print(item)
+            #        new_list_products.append(item)
+      #  print(new_list_products)
+
+      #  new_list_products = []
+       # for item in products:
+        #    if "Чорний" in item['colors']:
+         #       new_list_products.append(item)
+
+
+      #  print(new_list_products)
+
+        some_arr = [1, 2, 2, 3]
+        unique_val = []
+
+      #  for el in some_arr:
+       #     if el in unique_val:
+       #         continue
+      #      else:
+      #          unique_val.append(el)
+
+      #  print(unique_val)
+        
+
+
+
